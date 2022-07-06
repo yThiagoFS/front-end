b9c2be7f3c16fde19c9d7b7f82f3774ee8d355ff
Thiago Ferreira <99218990+yThiagoFS@users.noreply.github.com>
2022-07-05 22:06:33 -0300

    Adição das imagens tipo 50+

--- src/components/comunidade/postKairos/PostKairosCinquenta.jsx
+++ src/components/comunidade/postKairos/PostKairosCinquenta.jsx
@@ -1,11 +1,14 @@
 import PostKairos from "./PostKairos.jsx"
-import intercambioAuPair from "../img/imgIntercambioAuPair.jpg"
+import intercambio50 from "../img/50.jpg"
+import cinquentaFirsTipImg from "../img/requisitos-basicos-img-50.png"
+import disposicaoCinquenta from "../img/disposicao-cinquenta.png"
+import escolherLocalCinquenta from "../img/escolher-local-cinquenta.jpg
 
 export default function PostKairosCinquenta(){
 
     return(
         <PostKairos title ={` Intercâmbio +50`}
-        imgPost  ={intercambioAuPair}
+        imgPost  ={intercambio50}
         description ={`O intercâmbio Terceira Idade, conhecido como +50, são programas que combinam aulas de
         idiomas com atividades culturais e excursões, destinados aos aventureiros da melhor idade.
         Ótimo para quem quer aprender ou aperfeiçoar o idioma de maneira leve e descontraída e
@@ -15,14 +18,14 @@ export default function PostKairosCinquenta(){
         `}
         subtitle ={`O que é preciso para realizar o intercâmbio +50?`}
         firstTip ={`1 - Requisitos básicos`}
-        firstTipImg ={`http://seja1gm.com.br/wp-content/uploads/2019/10/EXIGENCIAS.png`}
+        firstTipImg ={cinquentaFirsTipImg}
         firstTipDescription ={`Ter mais que 50 anos, sem idade máxima.`}
         secondTip={`2 - Disposição`}
-        secondTipImg ={`https://www.estudaringlesonline.com.br/wp-content/uploads/2016/12/Head_English.png`}
+        secondTipImg ={disposicaoCinquenta}
         secondTipDescription ={`Disposto a ficar por mais de 2 semanas no programa de intercâmbio.
         `}
         thirdTip ={`3 - Escolher bem o local`}
-        thirdTipImg ={`https://azzolinadvogados.com.br/wp-content/uploads/2021/05/agendamento-carteira-de-trabalho-guia-completo-01-1024x512.png`}
+        thirdTipImg ={escolherLocalCinquenta}
         thirdTipDescription ={`Ver qual o tipo de idioma mais é falado, os tipos de passeio e atividades culturais de
         acordo com o destino e ver o tipo de hospedagem, como casa de família, apartamento ou
         hotel.`}/> 
