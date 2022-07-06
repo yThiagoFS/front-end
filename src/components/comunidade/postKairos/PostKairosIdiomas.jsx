1403491529e5b46ecb9f2de15baacb19997cc64d
Thiago Ferreira <99218990+yThiagoFS@users.noreply.github.com>
2022-07-05 22:32:19 -0300

    Imagens adicionadas intercâmbio de idiomas

--- src/components/comunidade/postKairos/PostKairosIdiomas.jsx
+++ src/components/comunidade/postKairos/PostKairosIdiomas.jsx
@@ -1,11 +1,14 @@
 import PostKairos from "./PostKairos.jsx"
-import intercambioAuPair from "../img/imgIntercambioAuPair.jpg"
+import imgIdioma from "../img/imgIdiomas.png"
+import idiomaRequisitos from "../img/idioma-requisitos.png
+import idiomaViagem from "../img/idioma-viagem.png"
+import idiomaPesquisar from "../img/idioma-pesquisar.png"
 
 export default function PostKairosIdiomas(){
 
     return(
         <PostKairos title ={`Intercâmbio de Idioma`}
-        imgPost  ={intercambioAuPair}
+        imgPost  ={imgIdioma}
         description ={`É uma viagem na qual o foco principal é com que tenha um conhecimento intercultural. Para
         adquirir fluência em outro idioma, estudar no exterior e fazer cursos de idioma é a melhor
         forma de atingir seu objetivo. Inclusive, é comprovado que o intercâmbio é uma das melhores
@@ -14,14 +17,14 @@ export default function PostKairosIdiomas(){
         perfeita para um aprendizado rápido e duradouro.`}
         subtitle ={`O que é preciso para realizar o intercâmbio de idioma?`}
         firstTip ={`1 - Requisitos básicos`}
-        firstTipImg ={`http://seja1gm.com.br/wp-content/uploads/2019/10/EXIGENCIAS.png`}
+        firstTipImg ={idiomaRequisitos}
         firstTipDescription ={`Escolher o tipo de curso que deseja fazer e o destino ideal para você.`}
         secondTip={`2 - Viagem`}
-        secondTipImg ={`https://www.estudaringlesonline.com.br/wp-content/uploads/2016/12/Head_English.png`}
+        secondTipImg ={idiomaViagem}
         secondTipDescription ={`Pesquisar as documentações necessárias para fazer o curso (como passaporte, visto, etc.).`}
         thirdTip ={`3 - Pesquisar
         `}
-        thirdTipImg ={`https://azzolinadvogados.com.br/wp-content/uploads/2021/05/agendamento-carteira-de-trabalho-guia-completo-01-1024x512.png`}
+        thirdTipImg ={idiomaPesquisar}
         thirdTipDescription ={`Fazer buscas sobre possíveis auxílios que vão te ajudar a conseguir essa oportunidade.`}/> 
         
        
